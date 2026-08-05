711ce648bf4535f65e4a722d8c6b6c05f61c3ef7
Aarav Shukla <104962175+Aaravshukla15@users.noreply.github.com>
2026-08-05 12:57:34 +0530

    Implemented Employee List and Create API

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    
+    # API
+    path("api/employees/", include("employees.urls")),
 ]
 
 if settings.DEBUG:
